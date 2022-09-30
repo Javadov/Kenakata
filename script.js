0baffee02a24434b8503bea0dc2ccb650ee62735
Elvin <javadov@hotmail.com>
2022-09-30 12:33:21 +0200

    All done

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function openNav() {
-    if (x.matches) { // If media query matches
+    if (x.matches) {                                                        // If media query matches
       document.getElementById("myNav").style.width = "50%";
     } else {
       document.getElementById("myNav").style.width = "70%";
     }
   }
   
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
+  function closeNav() {                                                     // Close when someone clicks on the "x" symbol inside the overlay
     document.getElementById("myNav").style.width = "0%";
   }
   var x = window.matchMedia("(min-width: 400px)")
